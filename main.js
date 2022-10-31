1a94a46a99f6baf2426eb198f3af1f860a0d0118
A1-MARTINEZ <116705619+A1-MARTINEZ@users.noreply.github.com>
2022-10-30 22:21:58 -0700

    Create main.js

---
+++ main.js
@@ -0,0 +1,42 @@
+// Magic 8 ball
+
+var userName = 'Alo';
+
+if (userName == null){
+    console.log(`Hello!`);
+} else {
+    console.log(`Hello ${userName}!`);
+}
+
+var userQuestion = 'Am i going to win the lottery?';{
+    console.log(userQuestion)
+}
+
+var randomNumber = Math.floor(Math.random() * 8);
+
+var eightBall = undefined;
+
+if (randomNumber === 1){
+    console.log(eightBall = 'It is certain')
+}
+if (randomNumber === 2){
+    console.log(eightBall = 'It is decidely so')
+}
+if (randomNumber === 3){
+    console.log(eightBall = 'Reply hazy try again')
+}
+if (randomNumber === 4){
+    console.log(eightBall = 'Cannot predict now')
+}
+if (randomNumber === 5){
+    console.log(eightBall = 'Do not count on it')
+}
+if (randomNumber === 6){
+    console.log(eightBall = 'My sources say no')
+}
+if (randomNumber === 7){
+    console.log(eightBall = 'Outlook not so good')
+}
+if (randomNumber === 8){
+    console.log(eightBall = 'Signs point to yes')
+}
